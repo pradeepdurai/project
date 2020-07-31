4b3cd014d57be8dbd0e7ec0dae2a28b88ea13c00
pradeepdurai <pradeepdurai66@gmail.com>
2020-07-30 21:34:31 -0400

    files includes

--- application/controllers/Index.php
+++ application/controllers/Index.php
@@ -1,27 +1,11 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+<?php 
 
-class Welcome extends CI_Controller {
-    public $data = array();
+class Index extends CI_Controller{
 
-    public function __construct()
-    {
-        parent::__construct();
+
+    public function index(){
+        
     }
-    
-	public function index($slug = 'home')
-	{
-        if($slug=='home')
-        {
-
-        }
-        $this->data['container'] = 'template/container/'.$slug;
-        array_push($this->data['styles'] , 'assets/css/'.$slug.'.css');
-        array_push($this->data['scripts'], 'assets/css/'.$slug.'.js');
-        $this->load->view('home',$data);
-	}
-
-	public function test(){
-		echo "HI";
-	}
 }
+
+?>
--- application/views/scripts.php
+++
@@ -1,8 +0,0 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
-<?php if (!empty($scripts) && count($scripts) > 0) : ?>
-    <?php foreach ($scripts as $script) : ?>
-        <?php if (!empty($script) && file_exists($script)) : ?>
-            <script src="<?php echo $script; ?>"></script>
-        <?php endif; ?>
-    <?php endforeach; ?>
-<?php endif; ?>
